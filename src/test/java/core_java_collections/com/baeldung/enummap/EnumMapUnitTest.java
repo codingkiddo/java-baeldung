a0d96139209530380944da2b6a7a0faf894115b9
codingkiddo <codingkiddo@gmail.com>
2024-12-18 08:21:10 +0530

    A Guide to EnumMap

--- src/test/java/core_java_collections/com/baeldung/enummap/EnumMapUnitTest.java
+++ src/test/java/core_java_collections/com/baeldung/enummap/EnumMapUnitTest.java
@@ -1,8 +1,24 @@
 package core_java_collections.com.baeldung.enummap;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
 public class EnumMapUnitTest {
 
 	public enum DayOfWeek {
 		MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 	}
+	
+	@Test
+    public void whenContructedWithEnumType_ThenOnlyAcceptThatAsKey() {
+        Map dayMap = new EnumMap<>(DayOfWeek.class);
+        assertThatCode(
+          () -> dayMap.put(TimeUnit.NANOSECONDS, "NANOSECONDS"))
+          .isInstanceOf(ClassCastException.class);
+    }
 }

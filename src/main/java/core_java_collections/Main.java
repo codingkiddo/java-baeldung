297df581cc1c79dc08566c332af4db81f6c67131
codingkiddo <codingkiddo@gmail.com>
2024-12-18 08:02:40 +0530

    A Guide to EnumMap

---
+++ src/main/java/core_java_collections/Main.java
@@ -0,0 +1,5 @@
+package core_java_collections;
+
+public class Main {
+
+}
---
+++ src/test/java/core_java_collections/com/baeldung/enummap/EnumMapUnitTest.java
@@ -0,0 +1,8 @@
+package core_java_collections.com.baeldung.enummap;
+
+public class EnumMapUnitTest {
+
+	public enum DayOfWeek {
+		MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+	}
+}

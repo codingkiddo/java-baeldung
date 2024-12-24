2b7fe5a967996b7ddc99a012931cd4089bf8a91d
codingkiddo <codingkiddo@gmail.com>
2024-12-24 19:26:29 +0530

    java baeldung examples

--- src/main/java/core_java_collections/Main.java
+++ src/main/java/core_java_collections/Main.java
@@ -1,5 +1,23 @@
 package core_java_collections;
 
-public class Main {
+import junit.runner.Version;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println(Version.id());
+		
+		try {
+//			getClass("core_java_collections.com.baeldung.collections.combiningcollections.CombiningArrays", Main.class);
+			getClass("junit.runner.Version", Main.class);
+			System.out.println("Found");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Not found");
+			e.printStackTrace();
+		}
+	}
+
+	public static Class<?> getClass(String className, Class<?> callingClass) throws ClassNotFoundException {
+		return Class.forName(className);
+	}
 }
---
+++ src/test/java/core_java_collections/com/baeldung/enummap/CombiningArraysUnitTest.java
@@ -0,0 +1,14 @@
+package core_java_collections.com.baeldung.enummap;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CombiningArraysUnitTest {
+    
+    @Test
+    public void givenTwoArrays_whenUsingNativeJava_thenArraysCombined() {
+        assertEquals("1", "1");
+    }
+    
+}
